31b5c8f791a2b02e5c0eb3262f2be16e5ffd741e
Jon Flaig <jon.flaig@graymeta.com>
2024-04-23 17:45:51 -0500

    Adding a new policy uncombining base policy and bucket policy

--- role/iam.tf
+++ role/iam.tf
@@ -21,29 +21,51 @@ resource "aws_iam_role" "iris" {
   assume_role_policy = data.template_file.iris_role.rendered
 }
 
+
 resource "aws_iam_instance_profile" "iris" {
   name = replace("${var.hostname_prefix}-${var.instance_type}-Profile", ".", "")
   role = aws_iam_role.iris.name
 }
+
+
+
 
 resource "aws_iam_policy" "iris_policy_base" {
   name   = replace("${var.hostname_prefix}-${var.instance_type}-Policy", ".", "")
   policy = data.template_file.iris_policy_base.rendered
 }
 
-resource "aws_iam_role_policy_attachment" "iris" {
-  policy_arn = aws_iam_policy.iris_combined.arn
-  role       = aws_iam_role.iris.name
+resource "aws_iam_policy" "iris_policy_custom" {
+  name   = replace("${var.hostname_prefix}-${var.instance_type}-BucketPolicy", ".", "")
+  policy = data.template_file.iris_policy_custom.rendered
 }
 
+
+
+/*
 resource "aws_iam_policy" "iris_combined" {
   name   = replace("${var.hostname_prefix}-${var.instance_type}-IAM-Policy", ".", "")
   policy = data.aws_iam_policy_document.combined.json
 }
+*/
 
+resource "aws_iam_role_policy_attachment" "iris_base" {
+  #policy_arn = aws_iam_policy.iris_combined.arn
+  policy_arn = aws_iam_policy.iris_policy_base.arn
+  role       = aws_iam_role.iris.name
+}
+
+resource "aws_iam_role_policy_attachment" "iris_bucket" {
+  #policy_arn = aws_iam_policy.iris_combined.arn
+  policy_arn = aws_iam_policy.iris_policy_custom.arn
+  role       = aws_iam_role.iris.name
+}
+
+/*
 data "aws_iam_policy_document" "combined" {
   source_policy_documents = [
     data.template_file.iris_policy_base.rendered,
     data.template_file.iris_policy_custom.rendered
   ]
 }
+*/
